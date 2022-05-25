ea2f1f24de9cbd176d7528165544e68186596c03
Kike Mendoza <kike.mendoza9978@gmail.com>
2022-05-24 22:07:32 -0500

    Implement first iteration reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringReversed = "";
+    longitud = string.length-1;
+    for(let i= 0; i<string.length;i++){
+        stringReversed += string.slice(longitud);
+        longitud --;
+    }
+    return stringReversed;
 };
 
 // Do not edit below this line
