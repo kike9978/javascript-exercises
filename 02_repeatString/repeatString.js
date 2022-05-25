ef8d0b7c4a81ff89f650041784e3e124b4e50355
Kike Mendoza <kike.mendoza9978@gmail.com>
2022-05-24 21:53:04 -0500

    Complete excercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let stringRepeated ="";
+    if(num < 0){
+        return stringRepeated= "ERROR";
+    }
+    for(let i=0; i<num;i++){
+       stringRepeated += string;
+    }
+    return stringRepeated;
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
