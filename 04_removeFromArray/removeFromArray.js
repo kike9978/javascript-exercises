6320010f91dd2fead4387b9688c700bcd82fd72f
Kike Mendoza <kike.mendoza9978@gmail.com>
2022-05-25 19:13:30 -0500

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (array,string) {
 
-};
+    let args = Array.prototype.slice.call(arguments,1);
+    console.log(args);
+    for(let i=0; i<args.length; i++){
+        for(const item of array){
+            if(item === args[i]){
+                array.splice(array.indexOf(item),1);
+            }
+        }
+    }
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
