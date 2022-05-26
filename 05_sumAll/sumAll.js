1bc23579ad8b5a9d5c399b45f4cc4f6d9601d211
Kike Mendoza <kike.mendoza9978@gmail.com>
2022-05-26 14:41:11 -0500

    Finish excercise 06, remeber the condition that forcebly executes de code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,32 +1,25 @@
 
 // ask for 2 integers
 const sumAll = function (num1, num2) {
-    // escape NaNs
+    // escape NaNs and bellow negatives
     if (num1 < 0 || num2 < 0 || typeof num1 != "number" || typeof num2 != "number") {
-        const message = "ERROR";
-        return message;
+        return "ERROR";
     }
     sum = 0;
+    // Check which is higher
     if (num1 > num2) {
-        for (let i = num2; i <= num1; i++) {
-            // add the number in the iteration to the sum
-            sum += i;
-        }
-    } else {
-        // loop betwen the first number given until the last
-        for (let i = num1; i <= num2; i++) {
-            // add the number in the iteration to the sum
-            sum += i;
-        }
+        const temp = num1;
+        num1 = num2;
+        num2 = temp;
     }
+    // loop betwen the first number given until the last
+    for (let i = num1; i <= num2; i++) {
+        // add the number in the iteration to the sum
+        sum += i;
+    }
+
     // return sum
     return sum;
-    // Check which is higher
-
-
-
-
-
 
 };
 
