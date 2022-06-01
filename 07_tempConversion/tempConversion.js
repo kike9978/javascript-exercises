d7eecf9d2fcc088344d6a3ccffa305af678d110f
Kike Mendoza <kike.mendoza9978@gmail.com>
2022-06-01 06:52:18 -0500

    Complete excercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,19 @@
-const ftoc = function() {
+const ftoc = function(f) {
+  c = (f - 32) * (5 / 9);
+  c = parseFloat(c.toFixed(1));
+  return c;
 
 };
 
-const ctof = function() {
+const ctof = function(c) {
+f = c * (9/5) + 32;
+f = parseFloat(f.toFixed(1));
+return f
 
 };
+
+
+// Return result fixed to one decimal
 
 // Do not edit below this line
 module.exports = {
